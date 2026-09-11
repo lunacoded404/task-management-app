b29177ab57dd7d7fc44afa0c1642a02886bbfb70
lunacoded404 <arichansts@gmail.com>
2026-09-12 03:21:33 +0700

    change firebase.py

--- backend/config/firebase.py
+++ backend/config/firebase.py
@@ -1,27 +1,3 @@
-# import os
-
-# import firebase_admin
-# from firebase_admin import credentials
-
-
-# BASE_DIR = os.path.dirname(
-#     os.path.dirname(
-#         os.path.abspath(__file__)
-#     )
-# )
-
-# firebase_service_account = os.path.join(
-#     BASE_DIR,
-#     "firebase-service-account.json"
-# )
-
-# cred = credentials.Certificate(
-#     firebase_service_account
-# )
-
-# firebase_admin.initialize_app(cred)
-
-import json
 import os
 
 import firebase_admin
@@ -34,16 +10,14 @@ BASE_DIR = os.path.dirname(
     )
 )
 
-firebase_config_str = os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON")
+firebase_service_account = os.path.join(
+    BASE_DIR,
+    "firebase-service-account.json"
+)
 
-if firebase_config_str:
-    cred_dict = json.loads(firebase_config_str)
-    cred = credentials.Certificate(cred_dict)
-else:
-    firebase_service_account = os.path.join(
-        BASE_DIR,
-        "firebase-service-account.json"
-    )
-    cred = credentials.Certificate(firebase_service_account)
+cred = credentials.Certificate(
+    firebase_service_account
+)
 
 firebase_admin.initialize_app(cred)
+

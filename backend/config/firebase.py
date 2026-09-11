828d90e4563783f80777a124704492fd727ba78c
lunacoded404 <arichansts@gmail.com>
2026-09-12 03:11:01 +0700

    Update firebase.py to load credentials from environment variables

--- backend/config/firebase.py
+++ backend/config/firebase.py
@@ -1,3 +1,27 @@
+# import os
+
+# import firebase_admin
+# from firebase_admin import credentials
+
+
+# BASE_DIR = os.path.dirname(
+#     os.path.dirname(
+#         os.path.abspath(__file__)
+#     )
+# )
+
+# firebase_service_account = os.path.join(
+#     BASE_DIR,
+#     "firebase-service-account.json"
+# )
+
+# cred = credentials.Certificate(
+#     firebase_service_account
+# )
+
+# firebase_admin.initialize_app(cred)
+
+import json
 import os
 
 import firebase_admin
@@ -10,13 +34,16 @@ BASE_DIR = os.path.dirname(
     )
 )
 
-firebase_service_account = os.path.join(
-    BASE_DIR,
-    "firebase-service-account.json"
-)
+firebase_config_str = os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON")
 
-cred = credentials.Certificate(
-    firebase_service_account
-)
+if firebase_config_str:
+    cred_dict = json.loads(firebase_config_str)
+    cred = credentials.Certificate(cred_dict)
+else:
+    firebase_service_account = os.path.join(
+        BASE_DIR,
+        "firebase-service-account.json"
+    )
+    cred = credentials.Certificate(firebase_service_account)
 
 firebase_admin.initialize_app(cred)
